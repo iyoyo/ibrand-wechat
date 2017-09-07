c7f6c699778d899418f2a3cd4b344c69ce2c18f8
XIAOXIAO229 <1317821900@qq.com>
2017-09-07 18:54:45 +0800

    1.修改小程序我的订单页面数据赋值方式。 2.完成小程序发票功能。 3.完成小程序商品列表按条件排序功能。 4.完成小程序搜索功能。

--- src/pages/store/invoice/invoice.js
+++ src/pages/store/invoice/invoice.js
@@ -1,11 +1,111 @@
+import {config} from '../../../lib/myapp.js'
 Page({
 	data: {
 		type: 'solo',
-		checked: []
+		token: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjQzNzM0ZTI5ZWRhZTM4MDNiY2FmZTNlNjhlMTUyOWNkYjJkYjVlZTViYzQyYzk0ZWRhZTEzY2JjMDhiZTE3NmFkOGY1NWIzZDFmODIwOWUxIn0.eyJhdWQiOiIxIiwianRpIjoiNDM3MzRlMjllZGFlMzgwM2JjYWZlM2U2OGUxNTI5Y2RiMmRiNWVlNWJjNDJjOTRlZGFlMTNjYmMwOGJlMTc2YWQ4ZjU1YjNkMWY4MjA5ZTEiLCJpYXQiOjE1MDM2MzkwNDcsIm5iZiI6MTUwMzYzOTA0NywiZXhwIjoxODE5MTcxODQ3LCJzdWIiOiI0OTUiLCJzY29wZXMiOltdfQ.j6JptbhqAheBqwZC4k4Fm9bd93oCCnGZDvwiHvFssvsIM-GIsKKIPA3gmeDoQaQRY2JeI3Tff1tz5uF1mwqbW_uexuPn80jicfvbGSljlrOkiU9s_rB1o20lLuZTc149it2x6IPAkDLSXIW3IZVOr7WQpyeM2gDgGBXeoV6OIjOggSpc1wKE25hEX2xhfQ7AyYrCihLbeCHqgSDxXEnS5MwY0XgV1vjd9yM6MyGaOFs05WDOhdeqf6I8gVRTT21dYjwM020-tWZMaHSJd3B6zhWHu_4V5Ql8tb3kP1jPgrPkeJhJgdRYWf_6Thiea32BsvEyCK2aT1vK03nOsj1kE78-SY52d6dTIg5syrwQyOgtq-KrmFw_EDCb_fZN-RCEgsGzSfajt984tDiI81-rFrx6jx9FfhS2tNut9ZqjtSctGhVrHY59CgSGjwDf-uLrZD7Ee0pAG2VhC4EYA9iZnr8oyw6Jxx4UIlWfh_-z8LHaglex1oRr_8cwUGkCvSrFXRojxobcxGDtjXW8o_tIhbgmkw57hle7wzdPFnyEIlR1Ap12bPtUhH7OyMCH9UTGTWXzUaW18UuH_-vrb1XUsv-fIm6BHQ4ic8824uUNVTTj4kRUQr99PCZ2K_9itvrDeETlgKbKXCpMjgO3f_t5ujv1DEemctxn76v9OHJ9pHg',
+		order_no: '',
+		content: ''
+	},
+	onLoad(e) {
+		this.setData({
+			order_no: e.order_no
+		})
 	},
 	change(e) {
 		this.setData({
 			type: e.detail.value
+		})
+	},
+	input(e) {
+		this.setData({
+			content: e.detail.value
+		})
+	},
+	submit() {
+		var type = '';
+		if (this.data.type == 'solo') {
+			type = '个人'
+		} else {
+			if (this.data.type != 'solo' && this.data.content == '') {
+				return wx.showToast({
+					title: '请输入单位名称',
+					image: '../../../assets/image/error.png'
+				})
+			} else {
+				type = this.data.content
+			}
+		}
+		this.addInvoiceInfo(type);
+	},
+	// 添加发票信息
+	addInvoiceInfo(type) {
+		wx.request({
+			url: config.GLOBAL.baseUrl + 'api/invoice-user/add',
+			header: {
+				Authorization: this.data.token
+			},
+			method: 'POST',
+			data: {
+				type: type
+			},
+			success: res => {
+				res = res.data;
+
+				if (res.status) {
+					var data = wx.getStorageSync('order_form');
+					if (!data) {
+						return wx.showToast({
+							title: '非法操作',
+							image: '../../../assets/image/error.png',
+							complete: err => {
+								setTimeout(() => {
+									wx.navigateBack();
+								},1600)
+							}
+						})
+					}
+
+					var order_no = this.data.order_no;
+					if (order_no && data.order_no === order_no) {
+						data.invoice = res.data;
+						wx.setStorageSync('order_form', data);
+						wx.navigateBack();
+					} else {
+						return wx.showToast({
+							title: '非法操作',
+							image: '../../../assets/image/error.png',
+							complete: err => {
+								setTimeout(() => {
+									wx.navigateBack();
+								},1600)
+							}
+						})
+					}
+				} else {
+					wx.showToast({
+						title: res.messages,
+						image: '../../../assets/image/error.png',
+						complete: err => {
+							setTimeout(() => {
+								wx.navigateBack();
+							},1600)
+						}
+					})
+				}
+			},
+			complete: err => {
+				if (err.statusCode != 200) {
+					wx.showToast({
+						title: '网络错误',
+						image: '../../../assets/image/error.png',
+						complete: err => {
+							setTimeout(() => {
+								wx.navigateBack();
+							},1600)
+						}
+					})
+				}
+			}
 		})
 	}
 
--- src/pages/store/list/list.js
+++ src/pages/store/list/list.js
@@ -3,27 +3,58 @@ import {config} from '../../../lib/myapp.js'
 Page({
 	data: {
 		page: 1,
-		storeList: {}
+		storeList: [],
+		orderBy: 'updated_at',
+		sort: 'desc',
+		c_id:'',
+		meta: '',
+		show: false
+	},
+	onLoad(e) {
+		if (e.c_id) {
+			this.setData({
+				c_id: e.c_id
+			})
+		}
+		if (e.orderBy) {
+			this.setData({
+				orderBy: e.orderBy,
+				sort: e.sort
+			})
+		}
+
 	},
 	onReady() {
-		console.log(config.GLOBAL.baseUrl)
 		wx.showLoading({
 			title: '加载中',
 			mask: true
 		});
-		wx.request({
-		    url: config.GLOBAL.baseUrl + "api/store/list",
-		    success:res=>{
-		    	res = res.data;
-		    	this.setData({
-				    storeList: res
-			    });
-			    wx.hideLoading()
-		    },
-			fail: err => {
-				wx.hideLoading()
-			}
-		});
+
+		var query = {
+			sort: this.data.sort,
+			orderBy: this.data.orderBy,
+			c_id: this.data.c_id
+		};
+
+		this.queryCommodityList(query);
+	},
+	changeOrderBy(e) {
+		var field = e.currentTarget.dataset.type;
+		if (this.data.orderBy === field) {
+			this.setData({
+				sort: this.data.sort === 'desc' ? 'asc' : 'desc'
+			})
+		} else {
+			this.setData({
+				orderBy: field,
+				sort: 'desc'
+			})
+		}
+
+		wx.redirectTo({
+			url: '/pages/store/list/list?orderBy=' + this.data.orderBy + '&sort=' + this.data.sort + '&c_id=' + this.data.c_id
+		})
+
 	},
 	jump(e) {
 		var id = e.currentTarget.dataset.id;
@@ -31,11 +62,65 @@ Page({
 			url: '/pages/store/detail/detail?id=' + id
 		})
 	},
+	search() {
+		wx.navigateTo({
+			url: '/pages/store/search/search'
+		})
+	},
 	onReachBottom() {
+		var hasMore = this.data.meta.pagination.total_pages > this.data.meta.pagination.current_page;
+		if (hasMore) {
+			this.setData({
+				show: true
+			})
+			var query = {
+				sort: this.data.sort,
+				orderBy: this.data.orderBy,
+				c_id: this.data.c_id
+			};
+			var page = this.data.meta.pagination.current_page + 1;
+			this.queryCommodityList(query,page);
+		} else {
+			wx.showToast({
+				title: '再拉也没有啦'
+			});
+		}
+
 	},
 	loadMore() {
 		wx.request({
 			url: config.GLOBAL.baseUrl + "api/store/list?page=" + (this.data.page + 1),
+		})
+	},
+	// 查询商品列表
+	queryCommodityList(query = {}, page = 1) {
+		var params = Object.assign({}, query, {page});
+
+
+		wx.request({
+			url: config.GLOBAL.baseUrl + 'api/store/list',
+			data: params,
+			success: res => {
+				res = res.data;
+				if (res.status) {
+					this.setData({
+						[`storeList.${page - 1}`]: res.data,
+						meta: res.meta
+					})
+				} else {
+					wx.showModal({
+						title: '',
+						content: res.message
+					})
+				}
+
+			},
+			complete: err => {
+				this.setData({
+					show: false
+				})
+				wx.hideLoading();
+			}
 		})
 	}
 })
